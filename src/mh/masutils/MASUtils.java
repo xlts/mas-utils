9b8b2703372f8bd0259d73c31139017cc92b29c2
Mariusz Hausenplas <xelitas@gmail.com>
2015-04-21 21:39:42 +0200

    poprawa isNull

--- src/mh/masutils/MASUtils.java
+++ src/mh/masutils/MASUtils.java
@@ -6,11 +6,12 @@ public class MASUtils {
 	public static boolean isNull(Object o) {
 		if (o == null) {
 			System.err.println("passed a null value");
-			return false;
-		}
-		else {
 			return true;
 		}
+		else {
+			return false;
+		}
 	}
+	
 }
 
